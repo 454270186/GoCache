17da06118b6c90a48b49380d8c760a4ea8e71673
454270186 <sc21ey@leeds.ac.uk>
2023-10-10 20:13:11 +0800

    fix some bugs

--- gocache/http.go
+++ gocache/http.go
@@ -31,7 +31,7 @@ type HTTPPool struct {
 	basePath    string
 	mu          sync.Mutex
 	peers       *consistenthash.HashRing // <data_key, peer_addr>
-	peersAddrs []string
+	peersAddrsSet map[string]bool
 	httpGetters map[string]*httpGetter   // <peer_addr, the_Get()>
 	httpPutters map[string]*httpPutter   // <peer_addr, the_Put()>
 }
@@ -42,6 +42,7 @@ func NewHTTPPool(addr string) *HTTPPool {
 		basePath:    DefaultBasePath,
 		httpGetters: make(map[string]*httpGetter),
 		httpPutters: make(map[string]*httpPutter),
+		peersAddrsSet: make(map[string]bool),
 	}
 }
 
@@ -156,13 +157,13 @@ func (h *HTTPPool) Set(addrs ...string) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	h.peersAddrs = addrs
-
 	h.peers = consistenthash.New(DefaultReplicas, nil)
 	h.peers.Add(addrs...)
 	for _, addr := range addrs {
 		h.httpGetters[addr] = &httpGetter{baseURL: addr + DefaultBasePath}
 		h.httpPutters[addr] = &httpPutter{baseURL: addr + DefaultBasePath}
+
+		h.peersAddrsSet[addr] = true
 	}
 }
 
@@ -194,7 +195,7 @@ func (h *HTTPPool) StartHealthCheck() {
 }
 
 func (h *HTTPPool) HealthCheck() {
-	for _, peerAddr := range h.peersAddrs {
+	for peerAddr := range h.peersAddrsSet {
 		log.Printf("[CheckHealth] Check %v\n", peerAddr)
 
 		ping := pb.Ping{
@@ -210,33 +211,44 @@ func (h *HTTPPool) HealthCheck() {
 		pingBodyBuffer := bytes.NewBuffer(pingBody)
 		resp, err := http.Post(peerAddr+HealthCheckPath, "", pingBodyBuffer)
 		if err != nil {
-			log.Printf("[Error] %v\n", err.Error())
-			return
+			log.Printf("[Unhealthy] peer %v: error while send health check: %v\n", peerAddr, err.Error())
+			h.peers.Remove(peerAddr)
+			delete(h.peersAddrsSet, peerAddr)
+			log.Printf("[Unhealthy] remove peer %v\n", peerAddr)
+			continue
 		}
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			log.Printf("[Unhealthy] peer %v\n", peerAddr)
+			log.Printf("[Unhealthy] peer %v\n: bad http resp code", peerAddr)
 			h.peers.Remove(peerAddr)
+			delete(h.peersAddrsSet, peerAddr)
 			log.Printf("[Unhealthy] remove peer %v\n", peerAddr)
 			continue
 		}
 		
 		bytes, err := io.ReadAll(resp.Body)
 		if err != nil {
-			log.Printf("[Error] error while read resp body: %v\n", err.Error())
-			return
+			log.Printf("[Unhealthy] peer %v: error while read resp body: %v\n", peerAddr, err.Error())
+			h.peers.Remove(peerAddr)
+			delete(h.peersAddrsSet, peerAddr)
+			log.Printf("[Unhealthy] remove peer %v\n", peerAddr)
+			continue
 		}
 
 		var pong pb.Pong
 		if err := proto.Unmarshal(bytes, &pong); err != nil {
-			log.Printf("[Error] error while unmarshal protobuf: %v\n", err.Error())
+			log.Printf("[Unhealthy] peer %v: error while unmarshal protobuf: %v\n", peerAddr, err.Error())
+			h.peers.Remove(peerAddr)
+			delete(h.peersAddrsSet, peerAddr)
+			log.Printf("[Unhealthy] remove peer %v\n", peerAddr)
 			return
 		}
 
 		if pong.PongCode != 1 {
-			log.Printf("[Unhealthy] peer %v\n", peerAddr)
+			log.Printf("[Unhealthy] peer %v: invalid PongCode\n", peerAddr)
 			h.peers.Remove(peerAddr)
+			delete(h.peersAddrsSet, peerAddr)
 			log.Printf("[Unhealthy] remove peer %v\n", peerAddr)
 			continue
 		}

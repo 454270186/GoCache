a69fbd1114eec7b38e8e1ef22bcdf8417141af7e
454270186 <sc21ey@leeds.ac.uk>
2023-10-10 20:18:31 +0800

    clean

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	// "fmt"
-
-	// gcache "github.com/454270186/GoCache/api"
 	"github.com/454270186/GoCache/server"
 )
 
 func main() {
-	// g := gcache.NewGoCache(
-	// 	"http://0.0.0.0:8002",
-	// 	"http://0.0.0.0:8003",
-	// )
-	// g.Put("xiaofei", "123")
-	// g.Put("feifei", "456")
-	// val, _ := g.Get("feifei")
-	// fmt.Println(val)
-
 	server.CacheServerMain()
 }
